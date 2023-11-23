2c315542863d9f0ef100edf40199dd2a93e6fdd3
Siddhant51 <siddhantsonawane37@gmail.com>
2023-11-24 01:57:20 +0530

    env

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
+require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(require("./routes"));
-require("dotenv").config();
 
 const port = process.env.PORT || 3001;
 
